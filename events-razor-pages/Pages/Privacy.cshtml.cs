d6025355a16144b6d7adf3086cfbf15ac508d63b
djurusic <djurusic@vub.hr>
2026-01-30 17:44:10 +0100

    inicijalni commit

---
+++ events-razor-pages/Pages/Privacy.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace events_razor_pages.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
+
